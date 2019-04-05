1bad1a3cd3a11b7a98ea42c8ea67b709e3a6becb
preciado75 <preciado75@yahoo.com>
2019-04-05 13:01:34 -0500

    servidor

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const directorioPublico = path.join(__dirname,'../public');
 const directorioPartials= path.join(__dirname,'../partials');
 const helpers = require('./helpers');
-const port=process.env.port||3000;
+const port=process.env.PORT||3000;
 app.use(express.static(directorioPublico));
 hbs.registerPartials(directorioPartials);
 app.use(bodyParser.urlencoded({extended:false}))
