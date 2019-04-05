05f5ea13eb280aa49fc10350825381b6d44884d9
preciado75 <preciado75@yahoo.com>
2019-04-05 13:13:35 -0500

    servidor

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const fs = require('fs');
 const directorioPublico = path.join(__dirname,'../public');
 const directorioPartials= path.join(__dirname,'../partials');
 const helpers = require('./helpers');
-const port=process.env.PORT||3000;
+//const port=process.env.PORT||3000;
 app.use(express.static(directorioPublico));
 hbs.registerPartials(directorioPartials);
 app.use(bodyParser.urlencoded({extended:false}))
@@ -115,7 +115,12 @@ app.get('*',(req,res) =>{
     })
 })
 
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  });
+  /*
 app.listen(port,() =>{
     console.log('Escuchando en el puerto '+port)
 });
 
+*/
